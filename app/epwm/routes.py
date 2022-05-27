6aa42c34810989be2463340c051e3e76d211d776
[jcitalan] <[joshcitalan@gmail.com]>
2022-05-27 10:04:35 -0600

    resto de enpoinst

--- app/epwm/routes.py
+++ app/epwm/routes.py
@@ -7,11 +7,7 @@ import os
 from shutil import rmtree
 import pandas as pd
 import numpy as np
-@epwm.get("/upload")
-def upload():
-  upload = UploadFile(request.form)
-  return render_template('upload.html',form=upload)
-  
+
 @cross_origin
 @epwm.post("/recibe")
 def recibe():
@@ -33,13 +29,13 @@ def recibe():
         os.remove(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'))
         df=pd.read_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))  
         df = df.fillna(0)
-        df.columns =['cadena', 'col1', 'col2', 'col3','col4','col5','col5']
+        df.columns =['cadena', 'col1', 'col2', 'col3','col4','col5','col6']
         df.to_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'),index = False)
       else:
         print("no existe ya el charts")
         df=pd.read_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))  
         df = df.fillna(0)
-        df.columns =['cadena', 'col1', 'col2', 'col3','col4','col5','col5']
+        df.columns =['cadena', 'col1', 'col2', 'col3','col4','col5','col6']
         df.to_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'),index = False)
       res = make_response(jsonify({"message":"recibido"}),200)
     else:
@@ -47,7 +43,7 @@ def recibe():
       f.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
       df=pd.read_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))   
       df = df.fillna(0)
-      df.columns =['cadena', 'col1', 'col2', 'col3','col4','col5','col5']
+      df.columns =['cadena', 'col1', 'col2', 'col3','col4','col5','col6']
       df.to_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'),index = False)
       res = make_response(jsonify({"message":"recibido"}),200)
   return res
@@ -55,29 +51,187 @@ def recibe():
 @cross_origin
 @epwm.get("/barchart")
 def barchart():
-  años=[]
+  anio=[]
+  scores=[]
   if(os.path.exists(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'))):
     dfn=pd.read_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'))
     titulo=dfn.iloc[0]['cadena'].lstrip().rstrip()
+    
+    
+    if '*' in str(dfn.iloc[13]['col2']):
+      uno = dfn.iloc[13]['col2']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col2'])
+
+    if '*' in str(dfn.iloc[13]['col3']):
+      uno = dfn.iloc[13]['col3']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col3'])
+
+    if '*' in str(dfn.iloc[13]['col4']):
+      uno = dfn.iloc[13]['col4']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col4'])
+
+    if '*' in str(dfn.iloc[13]['col5']):
+      uno = dfn.iloc[13]['col5']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col5'])
+
+    if '*' in str(dfn.iloc[13]['col6']):
+      uno = dfn.iloc[13]['col6']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(int(dfn.iloc[13]['col6']))
+    anio.sort()
+    
+    scores.append(int(dfn.iloc[17]['col2'])*int(dfn.iloc[18]['col2']))
+    scores.append(int(dfn.iloc[17]['col3'])*int(dfn.iloc[18]['col3']))
+    scores.append(int(dfn.iloc[17]['col4'])*int(dfn.iloc[18]['col4']))
+    scores.append(int(dfn.iloc[17]['col5'])*int(dfn.iloc[18]['col5']))
+    scores.append(int(dfn.iloc[17]['col6'])*int(dfn.iloc[18]['col6']))
+
+    print(scores)
+
     res = make_response(jsonify({
     "title":titulo,
-    "anios":[50000,50000,150000,248000,792000]
+    "anios":anio,
+    "scores":scores
     }),200)
   else:
     res = make_response(jsonify({
     "encabezado":"Sin datos",
-    "title":"Sin datos"
-    "anios":[50000,50000,150000,248000,792000]
+    "title":"Sin datos",
+    "anios":"Sin datos"
     }),200)
   return res
 
 @cross_origin
 @epwm.get("/linechart")
 def linechart():
-  res = make_response(jsonify({
+  anio=[]
+  scores=[]
+  gastos=[]
+  if(os.path.exists(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'))):
+    dfn=pd.read_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'))
+    titulo=dfn.iloc[0]['cadena'].lstrip().rstrip()
+
+    if '*' in str(dfn.iloc[13]['col2']):
+      uno = dfn.iloc[13]['col2']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col2'])
+
+    if '*' in str(dfn.iloc[13]['col3']):
+      uno = dfn.iloc[13]['col3']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col3'])
+
+    if '*' in str(dfn.iloc[13]['col4']):
+      uno = dfn.iloc[13]['col4']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col4'])
+
+    if '*' in str(dfn.iloc[13]['col5']):
+      uno = dfn.iloc[13]['col5']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col5'])
+
+    if '*' in str(dfn.iloc[13]['col6']):
+      uno = dfn.iloc[13]['col6']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(int(dfn.iloc[13]['col6']))
+    anio.sort()
+    
+    scores.append(int(dfn.iloc[17]['col2'])*int(dfn.iloc[18]['col2']))
+    scores.append(int(dfn.iloc[17]['col3'])*int(dfn.iloc[18]['col3']))
+    scores.append(int(dfn.iloc[17]['col4'])*int(dfn.iloc[18]['col4']))
+    scores.append(int(dfn.iloc[17]['col5'])*int(dfn.iloc[18]['col5']))
+    scores.append(int(dfn.iloc[17]['col6'])*int(dfn.iloc[18]['col6']))
+    
+    gastos.append(float(dfn.iloc[30]['col2'])+float(dfn.iloc[31]['col2'])+float(dfn.iloc[32]['col2'])+float(dfn.iloc[33]['col2'])+float(dfn.iloc[34]['col2'])+float(dfn.iloc[35]['col2'])+float(dfn.iloc[36]['col2'])+float(dfn.iloc[37]['col2'])+float(dfn.iloc[38]['col2'])+float(dfn.iloc[39]['col2'])+float(dfn.iloc[40]['col2'])+float(dfn.iloc[41]['col2'])+float(dfn.iloc[42]['col2'])+float(dfn.iloc[43]['col2'])+float(dfn.iloc[44]['col2'])+float(dfn.iloc[45]['col2'])+float(dfn.iloc[46]['col2']))
+    gastos.append(float(dfn.iloc[30]['col3'])+float(dfn.iloc[31]['col3'])+float(dfn.iloc[32]['col3'])+float(dfn.iloc[33]['col3'])+float(dfn.iloc[34]['col3'])+float(dfn.iloc[35]['col3'])+float(dfn.iloc[36]['col3'])+float(dfn.iloc[37]['col3'])+float(dfn.iloc[38]['col3'])+float(dfn.iloc[39]['col3'])+float(dfn.iloc[40]['col3'])+float(dfn.iloc[41]['col3'])+float(dfn.iloc[42]['col3'])+float(dfn.iloc[43]['col3'])+float(dfn.iloc[44]['col3'])+float(dfn.iloc[45]['col3'])+float(dfn.iloc[46]['col3']))
+    gastos.append(float(dfn.iloc[30]['col4'])+float(dfn.iloc[31]['col4'])+float(dfn.iloc[32]['col4'])+float(dfn.iloc[33]['col4'])+float(dfn.iloc[34]['col4'])+float(dfn.iloc[35]['col4'])+float(dfn.iloc[36]['col4'])+float(dfn.iloc[37]['col4'])+float(dfn.iloc[38]['col4'])+float(dfn.iloc[39]['col4'])+float(dfn.iloc[40]['col4'])+float(dfn.iloc[41]['col4'])+float(dfn.iloc[42]['col4'])+float(dfn.iloc[43]['col4'])+float(dfn.iloc[44]['col4'])+float(dfn.iloc[45]['col4'])+float(dfn.iloc[46]['col4']))
+    gastos.append(float(dfn.iloc[30]['col5'])+float(dfn.iloc[31]['col5'])+float(dfn.iloc[32]['col5'])+float(dfn.iloc[33]['col5'])+float(dfn.iloc[34]['col5'])+float(dfn.iloc[35]['col5'])+float(dfn.iloc[36]['col5'])+float(dfn.iloc[37]['col5'])+float(dfn.iloc[38]['col5'])+float(dfn.iloc[39]['col5'])+float(dfn.iloc[40]['col5'])+float(dfn.iloc[41]['col5'])+float(dfn.iloc[42]['col5'])+float(dfn.iloc[43]['col5'])+float(dfn.iloc[44]['col5'])+float(dfn.iloc[45]['col5'])+float(dfn.iloc[46]['col5']))
+    gastos.append(float(dfn.iloc[30]['col6'])+float(dfn.iloc[31]['col6'])+float(dfn.iloc[32]['col6'])+float(dfn.iloc[33]['col6'])+float(dfn.iloc[34]['col6'])+float(dfn.iloc[35]['col6'])+float(dfn.iloc[36]['col6'])+float(dfn.iloc[37]['col6'])+float(dfn.iloc[38]['col6'])+float(dfn.iloc[39]['col6'])+float(dfn.iloc[40]['col6'])+float(dfn.iloc[41]['col6'])+float(dfn.iloc[42]['col6'])+float(dfn.iloc[43]['col6'])+float(dfn.iloc[44]['col6'])+float(dfn.iloc[45]['col6'])+float(dfn.iloc[46]['col6']))
+    
+    print(gastos)
+    res = make_response(jsonify({
     "title":"Ingresos",
     "title2":"Gastos",
-    "ingresos_anios":[0,50000,150000,248000,792000],
-    "gasto_anios":[ '45,441.33',27330.00,94250.00,107300.00,117300.00]
+    "ingresos_anios":scores,
+    "gasto_anios":gastos
+    }),200)
+  else:
+    res = make_response(jsonify({
+    "title":"Sin Datos",
+    "title2":"Sin Datos",
+    "ingresos_anios":"Sin Datos",
+    "gasto_anios":"Sin Datos"
+    }),200)
+  return res
+
+@cross_origin
+@epwm.get("/doughnutchart")
+def doughnutchart():
+  anio=[]
+  disponible=[]
+  if(os.path.exists(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'))):
+    dfn=pd.read_excel(os.path.join(current_app.config['UPLOAD_FOLDER'], 'charts.xlsx'))
+    titulo=dfn.iloc[0]['cadena'].lstrip().rstrip()
+    
+    if '*' in str(dfn.iloc[13]['col2']):
+      uno = dfn.iloc[13]['col2']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col2'])
+
+    if '*' in str(dfn.iloc[13]['col3']):
+      uno = dfn.iloc[13]['col3']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col3'])
+
+    if '*' in str(dfn.iloc[13]['col4']):
+      uno = dfn.iloc[13]['col4']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col4'])
+
+    if '*' in str(dfn.iloc[13]['col5']):
+      uno = dfn.iloc[13]['col5']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(dfn.iloc[13]['col5'])
+
+    if '*' in str(dfn.iloc[13]['col6']):
+      uno = dfn.iloc[13]['col6']
+      anio.append(int(uno.split('*')[0]))
+    else:
+      anio.append(int(dfn.iloc[13]['col6']))
+    anio.sort()
+
+    disponible.append(dfn.iloc[59]['col2'])
+    disponible.append(dfn.iloc[59]['col3'])
+    disponible.append(dfn.iloc[59]['col4'])
+    disponible.append(dfn.iloc[59]['col5'])
+    disponible.append(dfn.iloc[59]['col6'])
+
+    res = make_response(jsonify({
+    "labels":anio,
+    "disponible":disponible
+    }),200)
+  else:
+    res = make_response(jsonify({
+    "labels":labels,
+    "gasto_anios":"Sin Datos"
     }),200)
   return res
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from flask_cors import CORS,cross_origin
 from app.epwm.routes import epwm
 import os
 app = Flask(__name__,static_folder='app/epwm/static')
-CORS(app,resources={r'/recibe','/barchart','/linechart'})
+CORS(app,resources={r'/recibe','/barchart','/linechart','/doughnutchart'})
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.register_blueprint(epwm)
 app.secret_key = 'S#perS3crEt_007'
@@ -12,4 +12,4 @@ path = os.getcwd()
 UPLOAD_FOLDER = os.path.join(path, 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if __name__=='__main__':
-  app.run(debug=True,port="5000",host="0.0.0.0")
+  app.run(debug=True,host="0.0.0.0")
